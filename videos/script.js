2961fbc52a29bb5740f92db84c3cef40e1176228
rayadajani <37849258+rayadajani@users.noreply.github.com>
2019-05-17 12:52:41 -0700

    Add files via upload

---
+++ videos/script.js
@@ -0,0 +1,18 @@
+function goMalachiWaldo(el){
+	document.location.href="https://drive.google.com/file/d/1Y2NMfuRueAvSn_lKxuuieE-r3p0BJXVq/view";
+}
+function goTrevorRyan(el){
+	document.location.href="https://drive.google.com/drive/folders/1bNKPUieUlPszxpYV7zlUZ_PhRMxvygSQ?usp=sharing ";
+}
+function goJoshuaWill(el){
+	document.location.href="https://docs.google.com/document/d/11TDZUSB5im08iwEV1GyGlyIFYbQiQdsLKeVFqK9EeDw/edit ";
+}
+function goTrinaDonyae(el){
+	document.location.href="https://drive.google.com/file/d/1oWmrtvfqA_X5eKDVKQKQU1InfS1mWRHq/view ";
+}
+function goSamLeah(el){
+	document.location.href="https://drive.google.com/file/d/1zV9Goe8uHO2m95mL_CDE-oX4335KQX6C/view";
+}
+function goAndreLane(el){
+	document.location.href="https://docs.google.com/document/d/1TwTm487lJgalytmZTJ_gp-BBConAaxPYfybMgi9BbMk/edit?usp=sharing";
+}
